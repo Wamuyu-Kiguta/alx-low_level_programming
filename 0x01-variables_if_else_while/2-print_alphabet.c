b1654c647993cdd9323e791b000c9b4a06e7361b
Wamuyu-Kiguta <kigutajane@gmail.com>
2023-03-17 13:47:35 -0700

    Print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char j = 'a';
+	while (j <= 'z')
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+return (0);
+}
