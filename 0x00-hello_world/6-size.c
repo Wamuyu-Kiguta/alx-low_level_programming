e1cc8c780ddb70c61f9b1409a77fd9fa37f5a8b0
Wamuyu-Kiguta <kigutajane@gmail.com>
2023-03-17 00:43:25 -0700

    Sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-printf("size of char: %lc byte(s)\n", sizeof(char));
+printf("size of char: %ld byte(s)\n", sizeof(char));
 printf("size of int: %ld byte(s)\n", sizeof(int));
 printf("size of long int: %ld byte(s)\n", sizeof(long int));
 printf("size of long long int: %ld byte(s)\n", sizeof(long long int));
