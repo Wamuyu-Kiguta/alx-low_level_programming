1a6a5ad86b1a55241076771f26261b90aee54c37
Wamuyu-Kiguta <kigutajane@gmail.com>
2023-03-17 15:41:22 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int a = 0;
+	for (a = 0; a <= 9; a++)
+	{
+	putchar(a + '0');
+	}
+	putchar('\n');
+return (0);
+}
