31277eb0a1e9582a8bd054bd1754cb257be36caf
Wamuyu-Kiguta <kigutajane@gmail.com>
2023-03-21 12:45:01 -0700

    Uppercase and Lowercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - prints if character is uppercase or not
+ * @c: - prints character c
+ * Return:1 if uppercase or lowercase and 0 otherwise
+ */
+int _isalpha(int c)
+{
+if (9(c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	return (1);
+else
+	return (0);
+}
