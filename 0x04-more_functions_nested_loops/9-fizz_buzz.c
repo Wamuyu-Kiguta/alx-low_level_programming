07d24a13c1c6a131892ef6f58882ab2f51871a5b
Wamuyu-Kiguta <kigutajane@gmail.com>
2023-03-23 22:00:55 -0700

    Square

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,11 +10,11 @@ int start = 1, end = 100;
 while (start <= 100)
 {
 if (start % 3 == 0)
-	printf("Fizz");
+printf("Fizz");
 if (start % 5 == 0)
-	printf("Buzz");
+printf("Buzz");
 if (start % 3 != 0 && start % 5 != 0)
-	printf("%d", start);
+printf("%d", start);
 
 	putchar(start == end ? '\n' : ' ');
 	start++;
