ed4096e7f4adce3c4e454314684604f1c387cd8d
Wamuyu-Kiguta <kigutajane@gmail.com>
2023-04-12 10:23:13 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -40,7 +40,7 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
-	for (j = 0; s2[j] != '\0'; j++)
+	for (j = 0; s2[j] != '\0'; j++, i++)
 		ptr[i] = s2[j];
 	ptr[i] = '\0';
 
