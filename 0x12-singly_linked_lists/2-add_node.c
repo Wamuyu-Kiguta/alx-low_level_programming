0ccc261a5507fad03a4dfccfa0a86b4984a55fd7
Wamuyu_Kiguta <kigutajane@gmail.com>
2023-04-28 05:57:12 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,8 +2,27 @@
 
 /**
  * *add_node - adds a new node at the beginning of a list_t list
+ * @head: double pointer to the list_t list
  * @str: node to duplicate
  * Return: address to the new element or NULL if it fails
  */
 
 list_t *add_node(list_t **head, const char *str)
+{
+	list_t *dup;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	dup = malloc(sizeof(list_t));
+	if (!dup)
+		return (NULL);
+
+	dup->str = strdup(str);
+	dup->len = len;
+	dup->next = (*head);
+	(*head) = dup;
+
+	return (*head);
+}
